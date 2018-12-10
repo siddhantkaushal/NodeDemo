50d5a64e204827ee578540d1786ce5457eba71c2
siddhantkaushal <45432348+siddhantkaushal@users.noreply.github.com>
2018-12-10 15:06:06 +0530

    Update email_controller.js

--- api/controller/email_controller.js
+++ api/controller/email_controller.js
@@ -8,14 +8,14 @@ module.exports.sendMail = function (req, res) {
     var transporter = nodemailer.createTransport(smtpTransport({
         service: 'gmail',
         auth: {
-            user: 'skaushal.fzk@gmail.com',
-            pass: 'candymanclassmate'
+            user: 'email id',
+            pass: 'password'
         },
         tls: { rejectUnauthorized: false }
     }));
 
     let mailOptions = {
-        from: 'skaushal.fzk@gmail.com', // sender address
+        from: 'email id', // sender address
         to: req.body.to, // list of receivers
         subject: req.body.subject, // Subject line
         text: req.body.body // plain text body
